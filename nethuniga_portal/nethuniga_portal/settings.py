0017213c7fe7f1fdb51044781364876927a37547
Hemalatha <hemaa0305@gmail.com>
2026-09-02 13:32:47 +0530

    Fix syntax in settings static configuration

--- nethuniga_portal/nethuniga_portal/settings.py
+++ nethuniga_portal/nethuniga_portal/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 STORAGES = {
